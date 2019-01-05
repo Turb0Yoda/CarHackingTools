1d2c7050e535098819ca517ee9e5fa57b2097582
Turb0Yoda <karanbir.ahuja@gmail.com>
2019-01-05 14:16:25 -0800

    Update workstationinstall.sh
    Trimmed down the script a lot for MATE.

--- workstationinstall.sh
+++ workstationinstall.sh
@@ -16,29 +16,16 @@ cd /tools || exit
 # Set Background Images
 mkdir -p images
 cd images || exit
-wget https://carhacking.tools/install/images/background.gif -O background.gif
-gsettings set org.gnome.desktop.background picture-uri "/tools/images/background.gif"
+cp -r ~/carhackingtools/Icons /tools/images
+gsettings set org.mate.background picture-filename "/tools/images/background.gif"
 cd .. || exit
 
 # Configure Desktop
-gsettings set org.gnome.desktop.background primary-color '#000000'
-gsettings set org.gnome.desktop.interface clock-show-date true
-gsettings set org.gnome.desktop.interface enable-animations false
-gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-gsettings set org.gnome.desktop.screensaver lock-enabled false
-gsettings set org.gnome.desktop.screensaver picture-uri '/dev/null'
-gsettings set org.gnome.desktop.screensaver primary-color '#000000'
-gsettings set org.gnome.desktop.screensaver secondary-color '#000000'
-gsettings set org.gnome.desktop.screensaver ubuntu-lock-on-suspend false
-gsettings set org.gnome.login-screen disable-user-list true
-gsettings set org.gnome.nautilus.desktop trash-icon-visible false
-gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Terminal.desktop', 'Cantact.desktop', 'ICSim.desktop', 'ICSimControls.desktop', 'SavvyCAN.desktop', 'wireshark.desktop', 'arduino.desktop', 'moserial.desktop']"
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
-gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
-gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
-gsettings set org.gnome.shell.extensions.dash-to-dock unity-backlit-items true
-gsettings set org.gtk.Settings.FileChooser sort-directories-first true
+gsettings set org.mate.background show-desktop-icons true
+gsettings set org.mate.background primary-color '#000000'
+gsettings set org.mate.desktop.datetime clock-show-date true
+gsettings set org.mate.screensaver lock-enabled false
+gsettings set org.mate.screensaver logout-enabled false
 
 #Disable Power Managment
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
