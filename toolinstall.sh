2d30d88880505aaae2e65ba5030d9abab17b1371
Turb0Yoda <karanbir.ahuja@protonmail.com>
2021-01-17 17:16:25 -0800

    Rebasing off updated jgamblin's stuff
    Don't try to reinvent the wheel folks

--- toolinstall.sh
+++ toolinstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#Modified carhacking.tools script
+# Ubuntu Car Hacking Workstation Setup
+# TODO: General CLean Up.
+
 set -e
 
 # Setup Tools Directory
@@ -13,10 +15,6 @@ sudo usermod -a -G dialout "$USER"
 # Update System
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-
-#fix for node errors
-#commented out since it started to cause issues rather than fixing previous issues
-#echo 'export PATH=$PATH:/usr/local/bin' >> $HOME/.bashrc
 
 #  Java Fixes
 sudo add-apt-repository -y ppa:linuxuprising/java
@@ -90,13 +88,6 @@ wget \
 wireshark \
 zlib1g-dev
 
-#fix for npm build errors
-#commented out since it started to cause issues rather than fixing previous issues
-#sudo ln -s /usr/bin/nodejs /usr/bin/node
-
-#Python Pip
-#sudo python -m pip uninstall pip  # this might need sudo
-#sudo apt install --reinstall python-pip
 
 # Starting Car Hacking Tool Installation
 
@@ -105,24 +96,29 @@ printf "\\n"
 
 # Bluelog
 # Read The Docs: https://github.com/MS3FGX/Bluelog
-#cd /tools || exit
-#git clone https://github.com/MS3FGX/Bluelog.git
-#cd Bluelog || exit
-#sudo make install
-#cd .. || exit
+git clone https://github.com/MS3FGX/Bluelog.git
+cd Bluelog || exit
+sudo make install
+cd .. || exit
 
 # Can-Utils:
 # Read The Docs: https://github.com/linux-can/can-utils
 # More Reading: # More Reading: https://discuss.cantact.io/t/using-can-utils/24
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y  can-utils
 
+# Canbus-utils
+# Read The Docs Here: https://github.com/digitalbond/canbus-utils
+# More Reading:  http://www.digitalbond.com/blog/2015/03/05/tool-release-digital-bond-canbus-utils/
+git clone https://github.com/digitalbond/canbus-utils
+chmod 770 -R canbus-utils
+cd canbus-utils || exit
+npm install
+cd .. || exit
+
 # Cantact-App
 # Read The Docs Here: https://github.com/linklayer/cantact-app/
-#git clone https://github.com/linklayer/cantact-app
-#cd cantact-app
-#ant build
-#cd cantact-app || exit
-cd /tools || exit
+mkdir -p cantact-app
+cd cantact-app || exit
 wget https://github.com/linklayer/cantact-app/releases/download/v0.3.0-alpha/cantact-v0.3.0-alpha.zip
 sudo unzip cantact-v0.3.0-alpha.zip
 sudo rm cantact-v0.3.0-alpha.zip
@@ -131,9 +127,7 @@ cd .. || exit
 # Caringcaribou
 # Read The Docs Here: https://github.com/CaringCaribou/caringcaribou
 pip3 install --user python-can
-cd /tools || exit
 git clone https://github.com/CaringCaribou/caringcaribou
-cd .. || exit
 
 # c0f
 # Read the Docs Here: https://github.com/zombieCraig/c0f
@@ -142,102 +136,47 @@ sudo gem install c0f
 # ICSim
 # Read The Docs Here: https://github.com/zombieCraig/ICSim
 # Quick Start:  ./setup_vcan.sh &&  ./icsim vcan0 && ./controls vcan0
-cd /tools || exit
 git clone https://github.com/zombieCraig/ICSim.git
-cd .. || exit
 
 # KatyOBD
 # Read The Docs Here:
-cd /tools || exit
 git clone https://github.com/YangChuan80/KatyOBD
 #Fix Typo in KatyOBD
 cd KatyOBD || exit
-###pip install obd
 sed -i 's/tkinter/Tkinter/g' KatyOBD.py
-cd .. || exit
-
-# Canbus-utils
-# Read The Docs Here: https://github.com/digitalbond/canbus-utils
-# More Reading:  http://www.digitalbond.com/blog/2015/03/05/tool-release-digital-bond-canbus-utils/
-cd /tools || exit
-git clone https://github.com/digitalbond/canbus-utils
-chmod 777 -R /tools/canbus-utils
-cd canbus-utils || exit
-npm install
 cd .. || exit
 
 # Kayak
 # Read The Docs Here: http://kayak.2codeornot2code.org/
 # To Install ./Kayak-1.0-SNAPSHOT-linux.sh --silent
-#cd /tools || exit
-#git clone git://github.com/dschanoeh/Kayak
-#cd Kayak
-#mvn install
-#cd .. || exit
+mkdir -p -p kayak
+cd kayak || exit
+curl http://kayak.2codeornot2code.org/Kayak-1.0-SNAPSHOT-linux.sh > Kayak-1.0-SNAPSHOT-linux.sh
+chmod +x Kayak-1.0-SNAPSHOT-linux.sh
+cd .. || exit
 
 # OBD-Monitor
-cd /tools || exit
 git clone https://github.com/dchad/OBD-Monitor
 cd OBD-Monitor || exit
 cd src|| exit
-make gui
-make server
-make simulator
-make ftests
 make stests
-make utests
+make server
+make ftests
 cd .. || exit
 cd .. || exit
+
 
 # Python-ODB
 # Read The Docs Here: https://python-obd.readthedocs.io/en/latest/
-sudo pip3 install --user pySerial
-cd /tools || exit
-git clone https://github.com/brendan-w/python-OBD
-cd python-OBD || exit
-sudo pip3 install obd
-cd .. || exit
-
-# PyOBD:
-# Fix This!
-# Backup: git clone https://github.com/Pbartek/pyobd-pi.git
-cd /tools || exit
-wget http://www.obdtester.com/download/pyobd_0.9.3.tar.gz
-sudo tar -xzvf pyobd_0.9.3.tar.gz
-sudo rm -rf pyobd_0.9.3.tar.gz
-cd .. || exit
-# SavvyCAN
-# Read The Docs Here: https://github.com/collin80/SavvyCAN
-
-# SavvyCan Install
-cd /tools || exit
-git clone https://github.com/collin80/SavvyCAN.git
-cd SavvyCAN || exit
-sudo /opt/Qt5.9.0/5.9/gcc_64/bin/qmake
-sudo make
-sudo make install
-sudo ./install
-cd .. || exit
+pip3 install --user pySerial
+pip3 install --user obd
 
 # Scantool
 # Read The Docs Here: https://samhobbs.co.uk/2015/04/scantool-obdii-car-diagnostic-software-linux
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y  scantool
 
-# Socketcand
-# Read The Docs Here: https://github.com/dschanoeh/socketcand
-cd /tools || exit
-git clone http://github.com/dschanoeh/socketcand.git
-cd socketcand || exit
-autoconf
-./configure --without-config
- make clean
-make
-sudo make install
-cd .. || exit
-
 # UDSim
 # Read The Docs Here: https://github.com/zombieCraig/UDSim
-cd /tools || exit
 git clone https://github.com/zombieCraig/UDSim
 cd UDSim/src || exit
 make
@@ -264,17 +203,21 @@ Categories=Utility
 StartupNotify=false
 EOF
 
+wget https://play-lh.googleusercontent.com/HaPn8O7TNLTDN5PbPw-Gd1aq3mvTfrudkh9ov3DqNQebQ8a5HVTaNxXsgMXWXY-TAA -O cantact.png
+
 cat << EOF > Cantact.desktop
 [Desktop Entry]
 Name=Cantact
 Type=Application
 Path=/tools/cantact-app/cantact/bin
 Exec=sudo -H /tools/cantact-app/cantact/bin/cantact
-Icon=/tools/images/Icons/cantact.png
+Icon=/tools/icons/cantact.png
 Terminal=true
 Categories=Utility
 StartupNotify=false
 EOF
+
+wget https://icon2.cleanpng.com/20180421/uue/kisspng-car-speedometer-computer-icons-odometer-dashboard-speedometer-background-5adabbb1c097f5.1976244315242843377889.jpg -O icsim.jpg
 
 cat << EOF > ICSim.desktop
 [Desktop Entry]
@@ -282,7 +225,7 @@ Name=ICSim
 Type=Application
 Path=/tools/ICSim/
 Exec=/tools/ICSim/icsim vcan0
-Icon=/tools/images/Icons/icsim.png
+Icon=/tools/icons/icsim.jpg
 Terminal=true
 Categories=Utility
 StartupNotify=false
@@ -294,23 +237,27 @@ Name=ICSim Controls
 Type=Application
 Path=/tools/ICSim/
 Exec=/tools/ICSim/controls vcan0
-Icon=/tools/images/Icons/icsim.png
+Icon=/tools/icons/icsim.jpg
 Terminal=true
 Categories=Utility
 StartupNotify=false
 EOF
 
+wget http://www.vhv.rs/dpng/d/457-4576489_kayak-white-kayak-icon-png-transparent-png.png -O kayak.png
+
 cat << EOF > KayakInstall.desktop
 [Desktop Entry]
 Name=Kayak Install
 Type=Application
-Path=/tools/Kayak
-Exec=/tools/Kayak/application/target/kayak/bin/kayak
-Icon=/tools/images/Icons/kayak.png
+Path=/tools/kayak
+Exec=/tools/kayak/Kayak-1.0-SNAPSHOT-linux.sh
+Icon=/tools/icons/kayak.png
 Terminal=true
 Categories=Utility
 StartupNotify=false
 EOF
+
+wget https://icon2.cleanpng.com/20180421/uue/kisspng-car-speedometer-computer-icons-odometer-dashboard-speedometer-background-5adabbb1c097f5.1976244315242843377889.jpg -O KatyOBD.jpg
 
 cat << EOF > KatyOBD.desktop
 [Desktop Entry]
@@ -318,21 +265,16 @@ Name=KatyOBD
 Type=Application
 Path=/tools/KatyOBD
 Exec=sudo -H python KatyOBD.py
-Icon=/tools/images/Icons/KatyOBD.png
+Icon=/tools/icons/KatyOBD.jpg
 Terminal=true
 Categories=Utility
 StartupNotify=false
 EOF
 
-sudo rm ~/Desktop/SavvyCAN.desktop
-sleep 15
 sudo chmod 755 ./*.desktop
-cp ./*.desktop ~/Desktop
+cp ./*.desktop ~/.local/share/applications
 cd .. || exit
 
-cd ~/Desktop || exit
+cd ~/.local/share/applications || exit
 sudo chmod 755 ./*.desktop
-ls -1 ~/Desktop/*.desktop | xargs -i gio set "{}" "metadata::trusted" yes || exit
 cd .. || exit
-
-printf "Make sure to hit f5 on the desktop so the desktop icon metadata will refresh."
